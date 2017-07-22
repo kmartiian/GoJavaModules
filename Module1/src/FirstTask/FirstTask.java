904e9f240cc24fad39c13d305b9c8a90721a5103
kmartiian <kmartiian@gmail.com>
2017-07-22 16:22:46 +0300

    1st commit of Module 1 with renewed .gitignore

---
+++ Module1/src/FirstTask/FirstTask.java
@@ -0,0 +1,14 @@
+package FirstTask;
+
+/**
+ * Created by Константин on 2017-06-24.
+ */
+public class FirstTask {
+    public static void main (String[] args) {
+        System.out.println("Как я проведу лето 2017.");
+
+        System.out.println("1. GoIT GoJava Core - обучение");
+
+        System.out.println("2. Отдых на море в июле.");
+    }
+}
